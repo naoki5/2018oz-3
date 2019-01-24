8018a6c9d1ac45ed3edf341a216d6485f4aee7c2
runandstaki <runandstaki@gmail.com>
2019-01-23 23:33:45 -0500

    456

--- 0003.js
+++ 0003.js
@@ -1,14 +1,4 @@
-function sayhello71(){{
+function sayhello71(){
 alert('Hello,' + 
 document.getElementById('who71').value + '!');
-}{
-  function sayhello71() { 
-//関数imgchange1()を3000ミリ秒間隔で呼び出す 
-  setInterval("imgchange12()",3000); } 
-  
-
-function imgchange12() {
-  document.getElementById('logo').src = './image/456.png';
-}
-}
 }
