39355addbf570a0608ff1b9fbc7809b57620eb99
runandstaki <runandstaki@gmail.com>
2019-01-24 22:29:29 -0500

    123

--- 0005.js
+++ 0005.js
@@ -1,5 +1,4 @@
 function add(){
-  var x =document.getElementById('old125').value;
-  alert(Number(2019)-Number(x));
-  
+  var x =document.getElementById('old1').value;
+  alert(Number(2019)-Number(x)+'才'); 
 }
