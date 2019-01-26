e7e37700902c6483eb933421ab9a5d9a552db013
runandstaki <runandstaki@gmail.com>
2019-01-26 05:44:18 -0500

    123

--- 0005.js
+++ 0005.js
@@ -1,5 +1,6 @@
 function add(){
   var x =document.getElementById('old1').value;
   var y ='今年で'
-  alert((y)+Number(2019)-Number(x)+'才'); 
+alert('今年で'+
+     document.getElementById(old1).value + '!');
 }
