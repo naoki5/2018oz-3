fd8903659fee019939ed94b62ecb98f229dae485
runandstaki <runandstaki@gmail.com>
2019-02-03 20:18:28 -0500

    123

---
+++ 0008.js
@@ -0,0 +1,5 @@
+﻿function abc2(){
+  var h =document.getElementById('both1').value;
+  var w = document.getElementById('both2').value;
+  alert('あなたのボディマス指数は'+(Number(w)/((Number(h)/100)*(Number(h)/100)))+'です。');
+}
---
+++ 0009.js
@@ -0,0 +1,9 @@
+﻿function sayhello68() {
+ alert('この電車は,'+document.getElementById('who68').innerHTML +'です！');
+}
+function taro68(){
+ document.getElementById('who68').innerHTML = '東上線';
+}
+function hanako68(){
+ document.getElementById('who68').innerHTML = '生越線';
+}
